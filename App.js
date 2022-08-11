068b8e9dc28f0235f49c6268a2513bc4a76bc4ad
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-08-11 18:25:06 +0900

    Animated

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import styled from "styled-components";
+import styled from "styled-components/native";
+import { Animated } from "react-native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5fcff;
+`;
+const Box = styled.TouchableOpacity`
+  width: 200px;
+  height: 200px;
+  background-color: #ff0000;
+  justify-content: center;
+  align-items: center;
+`;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  return null;
+  return (
+    <Container>
+      <AnimatedBox></AnimatedBox>
+    </Container>
+  );
 }
